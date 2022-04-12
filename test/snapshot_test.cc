a1ba6d188f4dfe9faed5c9398971460b79dc7e02
Dup4 <lyuzhi.pan@gmail.com>
2022-04-12 10:05:59 +0800

    chore: clean test file before run test

--- test/snapshot_test.cc
+++ test/snapshot_test.cc
@@ -1,10 +1,17 @@
 #include <gtest/gtest.h>
+#include <iostream>
 
 #include "snapshot/snapshot.h"
 
+using namespace std;
 using namespace snapshot;
 
-class SnapshotTest : public testing::Test {};
+class SnapshotTest : public testing::Test, public Snapshot {
+public:
+    static string GetPWD() {
+        return Snapshot::getPWD();
+    }
+};
 
 TEST_F(SnapshotTest, to_string_int) {
     EXPECT_EQ(StringUtility::ToString(int(1)), "1");
