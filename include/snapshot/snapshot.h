9e901ba658ba50bdb57395dcd5a52cf4d590121f
Dup4 <lyuzhi.pan@gmail.com>
2022-04-12 10:05:48 +0800

    feat: pwd

--- include/snapshot/snapshot.h
+++ include/snapshot/snapshot.h
@@ -1,8 +1,11 @@
 #ifndef SNAPSHOT_SNAPSHOT_H
 #define SNAPSHOT_SNAPSHOT_H
 
+#include <unistd.h>
+#include <array>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -109,7 +112,27 @@ private:
 
 class Snapshot {
 public:
-private:
+protected:
+    static std::string exec(const char* cmd) {
+        std::array<char, 128> buffer;
+        std::string result;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+
+        if (!pipe) {
+            throw std::runtime_error("popen() failed!");
+        }
+
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+
+        return result;
+    }
+
+    static std::string getPWD() {
+        return exec("pwd");
+    }
+
     static std::string getSnapshotsDirname() {
         return "__snapshots__";
     }
