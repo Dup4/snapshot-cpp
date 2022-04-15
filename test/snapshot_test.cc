5dbd1254d5c046d0eb9780759969b4096933bd54
Dup4 <lyuzhi.pan@gmail.com>
2022-04-15 18:02:59 +0800

    test: add SNAPSHOT_INLINE example

--- test/snapshot_test.cc
+++ test/snapshot_test.cc
@@ -10,6 +10,33 @@ class SnapshotTest : public testing::Test, public Snapshot {
 public:
 };
 
+class RawString {
+public:
+    std::string s;
+
+    friend ostream& operator<<(ostream& os, const RawString& r) {
+        os << r.s;
+        return os;
+    }
+};
+
 TEST_F(SnapshotTest, to_string_int) {
-    EXPECT_EQ(StringUtility::ToString(int(1)), "1");
+    const std::string a = R"({
+        "foo": bar
+})";
+
+    const std::string b = R"({
+        "foo": bar2
+})";
+
+    SNAPSHOT(a);
+
+    const char* snapshot_inline = "SNAPSHOT_INLINE(r)";
+
+    RawString r;
+    r.s = snapshot_inline;
+
+    SNAPSHOT_INLINE(r);
+
+    SNAPSHOT_DIFF(a, b);
 }
