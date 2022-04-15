4f3c6d909f8d9b7be25bd8fe263c0d00b8e4b347
Dup4 <lyuzhi.pan@gmail.com>
2022-04-15 17:32:39 +0800

    test: add remove_file case

--- test/file_utility_test.cc
+++ test/file_utility_test.cc
@@ -10,4 +10,26 @@ class FileUtilityTest : public testing::Test {
 public:
 };
 
-TEST_F(FileUtilityTest, get_all_lines) {}
+TEST_F(FileUtilityTest, mkdir) {
+    std::string s = "/tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4/a/b/c/d";
+
+    FileUtility::Mkdir(s);
+
+    EXPECT_EQ(0, access("/tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4/a", F_OK));
+    EXPECT_EQ(0, access("/tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4/a/b", F_OK));
+    EXPECT_EQ(0, access("/tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4/a/b/c", F_OK));
+    EXPECT_EQ(0, access("/tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4/a/b/c/d", F_OK));
+
+    system("rm -rf /tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4");
+}
+
+TEST_F(FileUtilityTest, remove_file) {
+    const char* file_path = "/tmp/340E2B12-25DD-4BB6-9A41-D24C8C4F76D4";
+    system((std::string("touch ") + file_path).c_str());
+
+    EXPECT_EQ(0, access(file_path, F_OK));
+
+    FileUtility::RemoveFile(file_path);
+
+    EXPECT_NE(0, access(file_path, F_OK));
+}
