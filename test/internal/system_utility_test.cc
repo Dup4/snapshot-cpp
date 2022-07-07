8a8299450300d86568243357fe6a26206a240eaf
Dup4 <lyuzhi.pan@gmail.com>
2022-07-07 13:31:39 +0800

    style: format

--- test/internal/system_utility_test.cc
+++ test/internal/system_utility_test.cc
@@ -23,7 +23,8 @@ TEST_F(SystemUtilityTest, diff) {
 
     auto diff = SystemUtility::Diff(file_a_path, file_b_path);
 
-    EXPECT_EQ(diff,
+    EXPECT_EQ(
+            diff,
             std::string(
                     R"(diff --git a/tmp/22220693-3FBD-4A2A-A97A-15A181CC7A67 b/tmp/22220693-3FBD-4A2A-A97A-15A181CC7A68
 index 72943a1..f761ec1 100644
--- test/internal/utility_test.cc
+++ test/internal/utility_test.cc
@@ -14,14 +14,14 @@ TEST_F(UtilityTest, all_and_any_and_count) {
     vector<int> v({1, 2, 3});
 
     EXPECT_EQ(true, Utility::All(v, [](const int x) {
-        return x >= 1;
-    }));
+                  return x >= 1;
+              }));
 
     EXPECT_EQ(true, Utility::Any(v, [](const int x) {
-        return x >= 2;
-    }));
+                  return x >= 2;
+              }));
 
     EXPECT_EQ(2, Utility::Count(v, [](const int x) {
-        return x >= 2;
-    }));
+                  return x >= 2;
+              }));
 }
